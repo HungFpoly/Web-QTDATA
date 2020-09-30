d92483504737c6fec1b5aa803a55fdf19641e472
dreamernguyen <55968031+dreamernguyen@users.noreply.github.com>
2020-09-30 13:17:52 +0700

    Update lần 5

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const expressHbs = require('express-handlebars');
+
+var app = express();
+app.listen(2020);
+app.use(express.static('css'));
+app.use(express.static('js'));
+app.use(express.static('images'));
+
+app.engine('.hbs', expressHbs());
+app.set('view engine', '.hbs');
+
+// app.get('/home',function(req,res){
+//     res.render('home')
+// })
+// app.get('/company',function(req,res){
+//     res.render('company')
+// })
+// app.get('/solutions',function(req,res){
+//     res.render('solutions')
+// })
+app.get('/qt-community',function(req,res){
+    res.render('qt-community')
+})
+
+
